fafe9a20461d8ebb1f99e08d86cd1cd6183b98e0
Villaroya <kevin.villaroya@gmail.com>
2022-07-30 15:05:51 +0200

    env in git ignore

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const path = require('path')
 
 require('dotenv').config({path:__dirname+'../../env/.local.env'});
 
+console.log(process.env.PORT);
+
 const server = require('./server');
 
 const port = process.env.PORT || 8080;
