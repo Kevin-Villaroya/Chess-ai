2291284a9fc1894ff261fa7ad459325f03cddc4d
Villaroya <kevin.villaroya@gmail.com>
2022-08-06 12:00:42 +0200

    default db

---
+++ db/import.sh
@@ -0,0 +1,3 @@
+mongo ChessAI --eval "db.dropDatabase()";
+mongoimport --db ChessAI --collection users --file users.json --jsonArray --drop
+mongoimport --db ChessAI --collection ai --file ai.json --jsonArray --drop
